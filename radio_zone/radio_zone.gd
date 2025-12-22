6127ced31f61045ea0f5ab942beb599c0d64f639
YeyoGSD <yeyoGSD@proton.me>
2025-12-22 14:51:07 -0700

    Arregla trigger de dialogo cuando Player usaba slide

--- radio_zone/radio_zone.gd
+++ radio_zone/radio_zone.gd
@@ -4,10 +4,10 @@ extends Area2D
 @export_multiline var dialogue_text := "Escribe aquí tu mensaje..."
 
 func _ready() -> void:
-	body_entered.connect(func(body: Node2D):
-		if body is Player:
+	area_entered.connect(func(area: Area2D):
+		if area.name == "BodySensor":
 			EventBus.radio_message_show_requested.emit(dialogue_text))
 	
-	body_exited.connect(func(body: Node2D):
-		if body is Player:
+	area_exited.connect(func(area: Area2D):
+		if area.name == "BodySensor":
 			EventBus.radio_message_hide_requested.emit())

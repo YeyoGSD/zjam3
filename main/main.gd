62bd26e02b3e27c06a1bbd158e5378da8f6e5498
YeyoGSD <yeyoGSD@proton.me>
2025-12-22 18:39:26 -0700

    Agrega meta base

--- main/main.gd
+++ main/main.gd
@@ -5,7 +5,14 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		#TODO: Manejar fin del juego
 		print("Game Over")
 
+
 func _on_world_limits_body_entered(body: Node2D) -> void:
 	if body is Player:
 		#TODO: Manejar muerte por caida
 		get_tree().call_deferred("reload_current_scene")
+
+
+func _on_win_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		#TODO: Manejar victoria
+		print("You Win")

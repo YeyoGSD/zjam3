199278792836928bc80a6bc973c8cb06eb8156bb
YeyoGSD <yeyoGSD@proton.me>
2025-12-22 18:23:16 -0700

    Agrega reinicio por caida del mapa

--- main/main.gd
+++ main/main.gd
@@ -4,3 +4,8 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "DayCycle":
 		#TODO: Manejar fin del juego
 		print("Game Over")
+
+func _on_world_limits_body_entered(body: Node2D) -> void:
+	if body is Player:
+		#TODO: Manejar muerte por caida
+		get_tree().call_deferred("reload_current_scene")
